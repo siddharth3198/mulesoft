2d1b3c36a949c05e625933717d9a55f36f001c3d
siddharth <siddhucool00@gmail.com>
2020-05-19 20:47:08 +0530

    uploading all files

---
+++ OneDrive/Desktop/mulesoft/New folder/mulejavaintegration/src/main/java/com/cg/mulesoft/MyFirst.java
@@ -0,0 +1,12 @@
+package com.cg.mulesoft;
+
+public class MyFirst {
+
+	public static String getName(String name) {
+	return "welcome to mulesoft"+name;
+}
+	public  String getNameAndAge(String name,String age) {
+		return "welcome to mulesoft"+name+"Age is"+age;
+	}
+	
+}
